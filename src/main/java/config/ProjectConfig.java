2a51f7917a77816b83a57ca5d3b68e7c219ca968
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-31 14:10:04 -0500

    Use @Autowired annotation to inject dependencies through the class fields

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,25 +1,9 @@
 package config;
 
-import beans.Parrot;
-import beans.Person;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "beans")
 public class ProjectConfig {
-
-    @Bean
-    public Parrot parrot() {
-        Parrot p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
-
-    @Bean
-    public Person person(Parrot parrot) {
-        Person p = new Person();
-        p.setName("Ella");
-        p.setParrot(parrot);
-        return p;
-    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import beans.Parrot;
 import beans.Person;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,14 +8,9 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Person person = context.getBean(Person.class);
+        Person p = context.getBean(Person.class);
 
-        Parrot parrot = context.getBean(Parrot.class);
-
-        System.out.println("Person's name: " + person.getName());
-
-        System.out.println("Parrot's name: " + parrot.getName());
-
-        System.out.println("Person's parrot: " + person.getParrot());
+        System.out.println("Person's name: " + p.getName());
+        System.out.println("Person's parrot: " + p.getParrot());
     }
 }
